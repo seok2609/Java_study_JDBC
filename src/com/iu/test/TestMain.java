cc8c0f2f1c582b0514a287153951e197b3fe832d
seok2609 <hjs120926@naver.com>
2022-07-27 12:20:27 +0900

    test

--- src/com/iu/test/TestMain.java
+++ src/com/iu/test/TestMain.java
@@ -1,9 +1,18 @@
 package com.iu.test;
 
+import com.iu.util.DBConnector;
+
 public class TestMain {
 	
 	public static void main(String[] args) {
+		DBConnector dbConnector = new DBConnector();
 		
+		try {
+			dbConnector.getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
